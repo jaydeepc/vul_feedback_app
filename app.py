36f809a4e2e5ed736b0b3d37133d087bc2168b4e
jaydeepc <jaydeepc@INjaydeepc-2.local>
2016-11-22 01:19:37 +0530

    JD: Added error handling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, json
 from flaskext.mysql import MySQL
 
+
 app = Flask(__name__)
 
 mysql = MySQL()
@@ -15,7 +16,12 @@ mysql.init_app(app)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('login.html')
+
+
+@app.errorhandler(500)
+def handle_internal_server_error(error):
+    return str(error)
 
 
 @app.route('/login',methods=['POST'])
@@ -23,15 +29,20 @@ def login():
     _username = request.form['username']
     _password = request.form['inputPassword']
 
-    # validate the received values
     if _username and _password:
         conn = mysql.connect()
         cursor = conn.cursor()
-        cursor.execute("select user_name from tbl_user where user_username='{0}'".format(_username))
-        data = cursor.fetchone()
+        query = "select user_name from tbl_user where user_username='{0}' and user_password='{1}'".format(_username, _password)
+        try:
+            cursor.execute(query)
+            data = cursor.fetchone()
+        except Exception as ex:
+            raise Exception(query, ex)
 
         if data is not None:
             return render_template('home.html')
+        else:
+            return "No user found"
 
     else:
         return json.dumps({'html':'<span>Enter the required fields</span>'})
@@ -39,5 +50,3 @@ def login():
 
 if __name__ == "__main__":
     app.run()
-
-    # select user_name from tbl_user where user_username='admin' or 1=1 '
